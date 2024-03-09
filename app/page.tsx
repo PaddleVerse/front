43793343f4862d29e83ad4586b81c96354769bd8
ybenlafk <yben0600@gmail.com>
2024-03-09 12:26:47 +0100

    change bg in login page

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import axios from "axios";
 import { useRouter } from 'next/navigation';
 import { set, useForm } from "react-hook-form";
 import { motion } from "framer-motion";
+import { Boxes } from "@/components/ui/background-boxes";
 
 
 import {
@@ -92,67 +93,69 @@ export default function SignupFormDemo() {
   }
 
   return (
-    <div className="w-full h-full flex items-center justify-center">
-      <div className="max-w-md lg:w-full w-[80%] mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-black ring-[0.2px] ring-red-500 z-10">
-          <motion.h1
-            className='text-red-500 lg:text-4xl md:text-3xl text-2xl font-semibold mb-2 sm:mb-0 text-center'
-          >
-            Login
-          </motion.h1>
+    <BgWrapper>
+      <div className="w-full h-full flex items-center justify-center">
+          <div className="max-w-md lg:w-full w-[80%] mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-black ring-[0.2px] ring-red-500 z-10">
+              <motion.h1
+                className='text-red-500 lg:text-4xl md:text-3xl text-2xl font-semibold mb-2 sm:mb-0 text-center'
+              >
+                Login
+              </motion.h1>
 
-        <form className="my-8" onSubmit={form.handleSubmit(onSubmit)}>
-          <LabelInputContainer className="mb-4">
-            <Label htmlFor="username">Username</Label>
-            <Input id="username" placeholder="Enter your username" type="text" {...form.register('username')}/>
-            {is === 3 && <p className="text-red-500 text-sm my-4">{error}</p>}
-          </LabelInputContainer>
-          <LabelInputContainer className="mb-4">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" placeholder="••••••••" type="password" {...form.register('password')}/>
-            {is === 4 && <p className="text-red-500 text-sm my-4">{error}</p>}
-          </LabelInputContainer>
-          <button
-            className="bg-gradient-to-br relative group/btn from-zinc-900 to-zinc-900 block bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-            type="submit"
-          >
-            Sign In &rarr;
-            <BottomGradient />
-          </button>
+            <form className="my-8" onSubmit={form.handleSubmit(onSubmit)}>
+              <LabelInputContainer className="mb-4">
+                <Label htmlFor="username">Username</Label>
+                <Input id="username" placeholder="Enter your username" type="text" {...form.register('username')}/>
+                {is === 3 && <p className="text-red-500 text-sm my-4">{error}</p>}
+              </LabelInputContainer>
+              <LabelInputContainer className="mb-4">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" placeholder="••••••••" type="password" {...form.register('password')}/>
+                {is === 4 && <p className="text-red-500 text-sm my-4">{error}</p>}
+              </LabelInputContainer>
+              <button
+                className="bg-gradient-to-br relative group/btn from-zinc-900 to-zinc-900 block bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+                type="submit"
+              >
+                Sign In &rarr;
+                <BottomGradient />
+              </button>
 
-          <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+              <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent my-8 h-[1px] w-full" />
 
-          <div className="flex flex-col space-y-4">
-            <button
-              className=" relative group/btn flex space-x-2 items-center justify-center px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-zinc-900"
-              type="button"
-              onClick={handle42}
-            >
-              <Image src="/Apple Logo.svg" className="w-5" alt="42" width={100} height={100} />
-              <span className="text-neutral-300 text-sm">
-                Intra 42
-              </span>
-              <BottomGradient />
-            </button>
-            <button
-              className=" relative group/btn flex space-x-2 items-center justify-center px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-zinc-900"
-              type="button"
-              onClick={handleGoogle}
-            >
-              <IconBrandGoogle className="h-4 w-4 text-neutral-300" />
-              <span className="text-neutral-300 text-sm">
-                Google
-              </span>
-              <BottomGradient />
-            </button>
+              <div className="flex flex-col space-y-4">
+                <button
+                  className=" relative group/btn flex space-x-2 items-center justify-center px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-zinc-900"
+                  type="button"
+                  onClick={handle42}
+                >
+                  <Image src="/Apple Logo.svg" className="w-5" alt="42" width={100} height={100} />
+                  <span className="text-neutral-300 text-sm">
+                    Intra 42
+                  </span>
+                  <BottomGradient />
+                </button>
+                <button
+                  className=" relative group/btn flex space-x-2 items-center justify-center px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-zinc-900"
+                  type="button"
+                  onClick={handleGoogle}
+                >
+                  <IconBrandGoogle className="h-4 w-4 text-neutral-300" />
+                  <span className="text-neutral-300 text-sm">
+                    Google
+                  </span>
+                  <BottomGradient />
+                </button>
+              </div>
+            </form>
+            <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+            <div className="w-full flex justify-between">
+              <Label className="text-gray-300 text-sm" >Don&apos;t have an account?</Label>
+              <Link href="/register" className="text-gray-300 text-sm" >Sign Up &rarr;</Link>
+            </div>
           </div>
-        </form>
-        <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent my-8 h-[1px] w-full" />
-        <div className="w-full flex justify-between">
-          <Label className="text-gray-300 text-sm" >Don&apos;t have an account?</Label>
-          <Link href="/register" className="text-gray-300 text-sm" >Sign Up &rarr;</Link>
-        </div>
       </div>
-    </div>
+    </BgWrapper>
   );
 }
 
@@ -179,122 +182,11 @@ const LabelInputContainer = ({
   );
 };
 
-
-
-// "use client";
-// import { zodResolver } from "@hookform/resolvers/zod";
-// import { motion } from "framer-motion";
-// import { Rajdhani } from "next/font/google";
-// import { useForm } from "react-hook-form";
-// import * as z from "zod";
-// const rajdhani = Rajdhani({
-//   subsets: ["latin"],
-//   weight: ["400", "500", "600", "700"],
-// });
-
-// import { Form } from "@/components/ui/form";
-// import FormElement from "./components/Sign/FormElement";
-// import SignButton from "./components/Sign/SignButton";
-// import SignSocials from "./components/Sign/SignSocials";
-
-// import { useEffect, useState } from "react";
-// import { useRouter } from 'next/navigation';
-
-// import axios from 'axios';
-
-// import {
-//   FormLinesAlready,
-//   FormLinesSignUp,
-// } from "./components/Sign/FormRest";
-
-// const FormSchema = z.object({
-//   username: z.string().min(2, {
-//     message: "Username must be at least 2 characters.",
-//   }),
-//   password: z.string().min(6, {
-//     message: "Password must be at least 6 characters.",
-//   }),
-// });
-
-
-// export default function InputForm() {
-
-//   const router = useRouter();
-
-//   const form = useForm<z.infer<typeof FormSchema>>({
-//     resolver: zodResolver(FormSchema),
-//     defaultValues: {
-//       username: "",
-//       password: "",
-//     },
-//   }); 
-
-//   function onSubmit(values: z.infer<typeof FormSchema>) {
-//     axios.post("http://localhost:8080/auth/login", {
-//       username: values.username,
-//       password: values.password
-//     }, {
-//       headers: {
-//         'Content-Type': 'application/json'
-//       }
-//     })
-//     .then(response => {
-//       const data = response.data;
-
-//       const accessToken = data.access_token;
-
-//       document.cookie = `access_token=${data.access_token}; path=/;`;
-
-//       // Check if access_token is present
-//       if (accessToken) {
-//         // Access token is present, make a request to the protected endpoint
-//         axios.get("http://localhost:8080/auth/protected", {
-//           headers: {
-//             'Authorization': `Bearer ${accessToken}`
-//           }
-//         })
-//         .then(response => {
-//           if (response.status === 200)
-//             router.push('/Dashboard');
-//           else
-//             console.log("Failed to authenticate with protected endpoint");
-//         })
-//         .catch(error => {
-//           console.log("Error during protected endpoint request", error);
-//         });
-//       }
-//     })
-//     .catch(error => {
-//       console.log("Error during login request", error);
-//     });
-//   }
-
-//   return (
-//     <div className="w-full h-full flex items-center justify-center">
-//       <div className="md:w-[480px] pb-[40px] sm:pb-[50px] bg-dashBack flex flex-col items-center rounded-2xl md:rounded-lg w-[310px] xm:w-[370px] select-none ring-[0.2px] ring-red-500">
-//         <Form {...form}>
-//           <form
-//             onSubmit={form.handleSubmit(onSubmit)}
-//             className="w-4/5 mt-5 space-y-6 sm:mt-10"
-//           >
-//             <motion.h1
-//               className={`${rajdhani.className} text-redValorant lg:text-4xl md:text-3xl text-2xl font-semibold mb-2 sm:mb-0`}
-//             >
-//               Login
-//             </motion.h1>
-//             {/* Username Field */}
-//             <FormElement form={form} placeholder={"username"} />
-//             {/* Password Field */}
-//             <FormElement form={form} placeholder={"password"} />
-//             <div className="w-full flex items-center justify-center">
-//               <SignButton p="SIGN-IN" />
-//             </div>
-//           </form>
-//         </Form>
-//         <FormLinesSignUp />
-//         <SignSocials />
-//         <FormLinesAlready des="Don't have an account?" n="REGISTER" url="/register"/>
-//       </div>
-//     </div>
-//   );
-// }
+export const BgWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full relative w-full overflow-hidden bg-black flex flex-col items-center justify-center">
+      <Boxes />
+      {children}
+    </div>
+  );
+}
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -7,7 +7,7 @@ import axios from "axios";
 import { useRouter } from 'next/navigation';
 import { set, useForm } from "react-hook-form";
 import { motion } from "framer-motion";
-
+import { BgWrapper } from "../page";
 
 import {
   IconBrandGithub,
@@ -80,78 +80,80 @@ export default function SignupFormDemo() {
   }
 
   return (
-    <div className="w-full h-full flex items-center justify-center">
-      <div className="max-w-md lg:w-full w-[80%] mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-black ring-[0.2px] ring-red-500 z-10">
-        <motion.h1
-            className='text-red-500 lg:text-4xl md:text-3xl text-2xl font-semibold mb-2 sm:mb-0 text-center'
-          >
-            Register
-          </motion.h1>
+    <BgWrapper>
+      <div className="w-full h-full flex items-center justify-center">
+        <div className="max-w-md lg:w-full w-[80%] mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-black ring-[0.2px] ring-red-500 z-10">
+          <motion.h1
+              className='text-red-500 lg:text-4xl md:text-3xl text-2xl font-semibold mb-2 sm:mb-0 text-center'
+            >
+              Register
+            </motion.h1>
 
-        <form className="my-8" onSubmit={form.handleSubmit(onSubmit)}>
-          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-            <LabelInputContainer>
-              <Label htmlFor="firstname">Name</Label>
-              <Input id="firstname" placeholder="Tyler" type="text" {...form.register('name')}/>
+          <form className="my-8" onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+              <LabelInputContainer>
+                <Label htmlFor="firstname">Name</Label>
+                <Input id="firstname" placeholder="Tyler" type="text" {...form.register('name')}/>
+              </LabelInputContainer>
+              <LabelInputContainer>
+                <Label htmlFor="lastname">Nickname</Label>
+                <Input id="lastname" placeholder="Durden" type="text" {...form.register('nickname')}/>
+              </LabelInputContainer>
+            </div>
+              {(is === 1 || is === 2) && <p className="text-red-500 text-sm my-4">{error}</p>}
+            <LabelInputContainer className="mb-4">
+              <Label htmlFor="username">Username</Label>
+              <Input id="username" placeholder="Tyler_durden" type="text" {...form.register('username')}/>
+              {is === 3 && <p className="text-red-500 text-sm my-4">{error}</p>}
             </LabelInputContainer>
-            <LabelInputContainer>
-              <Label htmlFor="lastname">Nickname</Label>
-              <Input id="lastname" placeholder="Durden" type="text" {...form.register('nickname')}/>
+            <LabelInputContainer className="mb-4">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" placeholder="••••••••" type="password" {...form.register('password')}/>
+              {is === 4 && <p className="text-red-500 text-sm my-4">{error}</p>}
             </LabelInputContainer>
-          </div>
-            {(is === 1 || is === 2) && <p className="text-red-500 text-sm my-4">{error}</p>}
-          <LabelInputContainer className="mb-4">
-            <Label htmlFor="username">Username</Label>
-            <Input id="username" placeholder="Tyler_durden" type="text" {...form.register('username')}/>
-            {is === 3 && <p className="text-red-500 text-sm my-4">{error}</p>}
-          </LabelInputContainer>
-          <LabelInputContainer className="mb-4">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" placeholder="••••••••" type="password" {...form.register('password')}/>
-            {is === 4 && <p className="text-red-500 text-sm my-4">{error}</p>}
-          </LabelInputContainer>
-          <button
-            className="bg-gradient-to-br relative group/btn from-zinc-900 to-zinc-900 block bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-            type="submit"
-          >
-            Sign up &rarr;
-            <BottomGradient />
-          </button>
+            <button
+              className="bg-gradient-to-br relative group/btn from-zinc-900 to-zinc-900 block bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+              type="submit"
+            >
+              Sign up &rarr;
+              <BottomGradient />
+            </button>
 
+            <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+
+            <div className="flex flex-col space-y-4">
+              <button
+                className=" relative group/btn flex space-x-2 items-center justify-center px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-zinc-900"
+                type="button"
+                onClick={handle42}
+              >
+                <Image alt="42" src="/Apple Logo.svg" className="w-5" width={100} height={100} />
+                <span className="text-neutral-300 text-sm">
+                  Intra 42
+                </span>
+                <BottomGradient />
+              </button>
+              <button
+                className=" relative group/btn flex space-x-2 items-center justify-center px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-zinc-900"
+                type="button"
+                onClick={handleGoogle}
+              >
+                <IconBrandGoogle className="h-4 w-4 text-neutral-300" />
+                <span className="text-neutral-300 text-sm">
+                  Google
+                </span>
+                <BottomGradient />
+              </button>
+            </div>
+          </form>
           <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent my-8 h-[1px] w-full" />
-
-          <div className="flex flex-col space-y-4">
-            <button
-              className=" relative group/btn flex space-x-2 items-center justify-center px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-zinc-900"
-              type="button"
-              onClick={handle42}
-            >
-              <Image alt="42" src="/Apple Logo.svg" className="w-5" width={100} height={100} />
-              <span className="text-neutral-300 text-sm">
-                Intra 42
-              </span>
-              <BottomGradient />
-            </button>
-            <button
-              className=" relative group/btn flex space-x-2 items-center justify-center px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-zinc-900"
-              type="button"
-              onClick={handleGoogle}
-            >
-              <IconBrandGoogle className="h-4 w-4 text-neutral-300" />
-              <span className="text-neutral-300 text-sm">
-                Google
-              </span>
-              <BottomGradient />
-            </button>
+          <div className="w-full flex justify-between">
+            <Label className="text-gray-300 text-sm" >Already have an account?</Label>
+            <Link href='/' className="text-gray-300 text-sm" >Sign In &rarr;</Link>
           </div>
-        </form>
-        <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent my-8 h-[1px] w-full" />
-        <div className="w-full flex justify-between">
-          <Label className="text-gray-300 text-sm" >Already have an account?</Label>
-          <Link href='/' className="text-gray-300 text-sm" >Sign In &rarr;</Link>
         </div>
       </div>
-    </div>
+    </BgWrapper>
   );
 }
 
