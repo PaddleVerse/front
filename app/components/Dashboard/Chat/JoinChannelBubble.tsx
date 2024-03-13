4e26e475a2bb954a03d19f0f7afc60da6dc1316d
Mohamed Jarboua <mjarboua@e1r7p4.1337.ma>
2024-03-13 19:57:16 +0000

    aaded types for the joinchannelbubble

--- app/components/Dashboard/Chat/JoinChannelBubble.tsx
+++ app/components/Dashboard/Chat/JoinChannelBubble.tsx
@@ -1,70 +1,54 @@
 import { channel, user } from "@/app/Dashboard/Chat/type";
+import  Image  from "next/image";
 import axios from "axios";
 import React, { useState } from "react";
 import { GoLock } from "react-icons/go";
-
-interface TooltipProps {
-  content: string;
-  children: React.ReactNode;
-  // Add more props as needed for customization
-}
-
-// const Tooltip: React.FC<TooltipProps> = ({ content, children }) => {
-//   return (
-//     <div className="tooltip-container" title={content}>
-//       {children}
-//     </div>
-//   );
-// };
+import { Socket } from "socket.io-client";
 
 const JoinChannelBubble = ({
   lock,
   channel,
   user,
-  socket
+  socket,
 }: {
   lock: boolean;
   channel: channel;
-    user: user;
-    socket: any;
+  user: user;
+  socket: Socket;
 }) => {
-  const [hover, setHover] = useState(false);
-
-
   const handleClick = async () => {
-    const participantObject = {
-
-    };
-    const res = await axios.post("http://localhost:8080/participants", participantObject);
-    
+    const participantObject = {};
+    const res = await axios.post(
+      "http://localhost:8080/participants",
+      participantObject
+    );
     if (res.status === 200) {
-      // socket.emit("joinRoom", { room: channel.name, user: user })
+      //here we emit to the server
       console.log("Channel joined");
     }
-  }
+  };
 
   return (
-    <div className="flex ga-2 items-center col-start text-inherit relative" onClick={(e)=> console.log('clicked to join', channel.state)}>
-      <img
+    <div
+      className="flex ga-2 items-center col-start text-inherit relative"
+      onClick={(e) => console.log("clicked to join", channel.state)}
+    >
+      <Image
         src="/badge1.png"
         alt="image"
         className="lg:w-[95px] lg:h-[95px] md:w-[80px] md:h-[80px]"
       />
       <div
         className="flex flex-col gap-1"
-        onMouseEnter={(e) => setHover(true)}
-        onMouseLeave={(e) => setHover(false)}
         title={channel.topic}
       >
         <h2 className="2xl:text-md xl:text-[15px] md:text-[14px]">
           {channel.name}
         </h2>
-        {/* <Tooltip content={channel.topic}> */}
-          <p className="text-gray-400 xl:text-sm truncate md:text-xs lg:max-w-full md:max-w-[120px]">
-            {channel.topic.substring(0, 30) +
-              (channel.topic.length > 30 && " ...")}
-          </p>
-        {/* </Tooltip> */}
+        <p className="text-gray-400 xl:text-sm truncate md:text-xs lg:max-w-full md:max-w-[120px]">
+          {channel.topic.substring(0, 30) +
+            (channel.topic.length > 30 && " ...")}
+        </p>
       </div>
       {lock && (
         <GoLock className="absolute top-6 2xl:right-[91px] xl:right-[41px] lg:right-[35px] text-white hidden md:text-[14px] 2xl:text-[16px] lg:flex" />
