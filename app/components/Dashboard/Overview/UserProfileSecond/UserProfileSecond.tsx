553c8ee75044121d884864d0def7be20bf223a38
ybenlafk <yben0600@gmail.com>
2024-03-13 12:38:26 +0000

    add Skeleton

--- app/components/Dashboard/Overview/UserProfileSecond/UserProfileSecond.tsx
+++ app/components/Dashboard/Overview/UserProfileSecond/UserProfileSecond.tsx
@@ -22,15 +22,25 @@ const UserProfileSecond = ({ user }: any) => {
         <div className="w-full  h-[290px] relative">
           <PinContainer title="Overview" href="https://twitter.com/mannupaaji">
             <div className="overflow-hidden h-[290px] w-full">
-              <Image
-                src={user.banner_picture ? user.banner_picture : "/car1.jpg"}
-                fill
-                priority
-                style={{ objectFit: "cover" }}
-                alt="bg"
-                sizes="auto"
-                className="z-[-1] rounded-2xl"
-              />
+              {
+                user ? (
+                  <Image
+                    src={user?.banner_picture ? user?.banner_picture : "/car1.jpg"}
+                    fill
+                    priority
+                    style={{ objectFit: "cover" }}
+                    alt="bg"
+                    sizes="auto"
+                    className="z-[-1] rounded-2xl"
+                  />
+                ) : (
+                  <div className="flex items-center justify-center w-full h-full bg-gray-400 rounded  animate-pulse">
+                      <svg className="w-10 h-10 text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                          <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
+                      </svg>
+                  </div>
+                )
+              }
             </div>
           </PinContainer>
           <div
@@ -48,24 +58,42 @@ const UserProfileSecond = ({ user }: any) => {
               }}
             >
               <div className="w-full h-[60%]   relative">
-                <Image
-                  src={user.picture ? user.picture : "/b.png"}
-                  alt="profile"
-                  fill
-                  style={{ objectFit: "cover" }}
-                  sizes="auto"
-                  className="rounded-md"
-                />
+                {user ? (
+                  <Image
+                    src={user?.picture ? user?.picture : "/b.png"}
+                    alt="profile"
+                    fill
+                    style={{ objectFit: "cover" }}
+                    sizes="auto"
+                    className="rounded-md"
+                  />
+                ) : (
+                  <div className="flex items-center justify-center w-full h-full bg-gray-400 rounded  animate-pulse">
+                      <svg className="w-10 h-10 text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                          <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
+                      </svg>
+                  </div>
+                )}
               </div>
-              <div className="flex flex-col items-center">
-                <h2 className="mt-2 xl:text-[15px] text-[10px] text-center">
-                  {user.name}
-                </h2>
-                <span className="xl:text-[10px] text-[7px]">
-                  @{user.username}
-                </span>
-                <span className="xl:text-[10px] text-[7px]">400,000</span>
-              </div>
+              {
+                user ? (
+                <div className="flex flex-col items-center">
+                  <h2 className="mt-2 xl:text-[15px] text-[10px] text-center">
+                    {user.name}
+                  </h2>
+                  <span className="xl:text-[10px] text-[7px]">
+                    @{user.username}
+                  </span>
+                  <span className="xl:text-[10px] text-[7px]">400,000</span>
+                </div>
+                ) :(
+                <div  className="w-[80%] animate-pulse">
+                    <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-full mb-4"></div>
+                    <div className="h-2 bg-gray-300 rounded-full dark:bg-gray-700 w-full mb-2.5"></div>
+                    <div className="h-2 bg-gray-300 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                </div>
+                )
+              }
             </div>
           </div>
         </div>
