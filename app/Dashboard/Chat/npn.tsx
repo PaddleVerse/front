d37f3e14853f551105303a9cc15497ae06459dd9
Mohamed Jarboua <mjarboua@e1r6p5.1337.ma>
2024-04-02 21:19:42 +0000

    trying to connect the main page with sockets

--- app/Dashboard/Chat/npn.tsx
+++ app/Dashboard/Chat/npn.tsx
@@ -18,29 +18,6 @@ import {
 } from "@tanstack/react-query";
 const inter = Inter({ subsets: ["latin"] });
 
-const queryClient = new QueryClient();
-
-// const fetchChatList = async (userId: string) => {
-//   const res = await axios.get(`http://localhost:8080/chat/chatlist/${userId}`);
-//   const mapper = res.data.map((value: any) => {
-//     if (value.user) {
-//       const fetchMessage = async () => {
-//         return await axios.get(
-//           `http://localhost:8080/conversations/lastMessage?uid1=${value.id}&uid2=${self.id}`
-//         );
-//       }
-//       const results = fetchMessage();
-//       return {...value.data, msg: results};
-//     } else {
-//       const res = await axios.get(
-//         `http://localhost:8080/channels/messages/lastMessage/${value.id}?uid=${self.id}`
-//       );
-//       return res.data;
-//     }
-    
-//   });
-//   return res.data;
-// };
 
 const fetchChatList = async (userId: string) => {
   const res = await axios.get(`http://localhost:8080/chat/chatlist/${userId}`);
@@ -80,11 +57,6 @@ const Page = ({ children }: { children: React.ReactNode }) => {
   });
   const clt = useQueryClient();
 
-
-
-
-
-
   useEffect(() => {
     !show && setShowMessage(false);
   }, [show]);
@@ -105,10 +77,13 @@ const Page = ({ children }: { children: React.ReactNode }) => {
   }, [handleEscapeKeyPress]);
 
   useEffect(() => {
-    socket?.on("update", (data: any)=> {
-      
-    })
-  }, [socket])
+    socket?.on("update", (data: any) => {
+      clt.invalidateQueries({queryKey: ["chatList"]});
+    });
+    return () => {
+      socket?.off("update");
+    };
+  }, [socket]);
 
   useEffect(() => {
     socket?.on("ok", (data: any) => {
@@ -148,112 +123,112 @@ const Page = ({ children }: { children: React.ReactNode }) => {
 
   return (
     // <QueryClientProvider client={queryClient}>
-      <div className="w-[91%] mx-auto lg:h-full md:h-[92%] relative h-[80%] flex justify-center mt-5 overflow-hidden">
-        <AnimatePresence>
-          {modlar ? (
-            <JoinChannel handleClick={handleClick} user={state.user} />
-          ) : createModlar ? (
-            <CreateChannel handleClick={() => setCreateModlar(false)} />
-          ) : null}
-        </AnimatePresence>
-        <div className="lg:max-h-[95%] lg:w-[91%] w-full h-full ">
-          <div
-            className={`h-full w-full flex antialiased text-gray-200 bg-transparent rounded-xl ${inter.className}`}
-            style={{
-              backdropFilter: "blur(20px)",
-              backgroundColor: "rgba(13, 9, 10, 0.7)",
-            }}
-          >
-            <div className="flex-1 flex flex-col ">
-              <main className="flex-grow flex flex-row min-h-0">
-                <motion.section
-                  className={` flex flex-col flex-none overflow-auto ${
-                    showMessage && tablet ? "invisible" : "visible"
-                  } group lg:max-w-[300px] md:w-2/5 no-scrollbar`}
-                  initial={{ display: "flex", width: "100%", opacity: 1 }}
-                  animate={{
-                    display: showMessage && tablet ? "hidden" : "flex",
-                    width: showMessage && tablet ? "0" : "100%",
-                    opacity: showMessage && tablet ? 0 : 1,
-                    transition: { duration: 0.25 },
+    <div className="w-[91%] mx-auto lg:h-full md:h-[92%] relative h-[80%] flex justify-center mt-5 overflow-hidden">
+      <AnimatePresence>
+        {modlar ? (
+          <JoinChannel handleClick={handleClick} user={state.user} />
+        ) : createModlar ? (
+          <CreateChannel handleClick={() => setCreateModlar(false)} />
+        ) : null}
+      </AnimatePresence>
+      <div className="lg:max-h-[95%] lg:w-[91%] w-full h-full ">
+        <div
+          className={`h-full w-full flex antialiased text-gray-200 bg-transparent rounded-xl ${inter.className}`}
+          style={{
+            backdropFilter: "blur(20px)",
+            backgroundColor: "rgba(13, 9, 10, 0.7)",
+          }}
+        >
+          <div className="flex-1 flex flex-col ">
+            <main className="flex-grow flex flex-row min-h-0">
+              <motion.section
+                className={` flex flex-col flex-none overflow-auto ${
+                  showMessage && tablet ? "invisible" : "visible"
+                } group lg:max-w-[300px] md:w-2/5 no-scrollbar`}
+                initial={{ display: "flex", width: "100%", opacity: 1 }}
+                animate={{
+                  display: showMessage && tablet ? "hidden" : "flex",
+                  width: showMessage && tablet ? "0" : "100%",
+                  opacity: showMessage && tablet ? 0 : 1,
+                  transition: { duration: 0.25 },
+                }}
+              >
+                <div className=" p-4 flex-none mt-4">
+                  <p
+                    className={`text-2xl font-bold md:block group-hover:block mb-4`}
+                  >
+                    Messages
+                  </p>
+                  <form onSubmit={(e) => e.preventDefault()}>
+                    <div className="relative sm:block hidden">
+                      <label>
+                        <input
+                          className="rounded-lg py-2 pr-6 pl-10 w-full bg-white focus:outline-none text-black focus:shadow-md transition duration-300 ease-in"
+                          type="text"
+                          placeholder="Search Messenger"
+                        />
+                        <span className="absolute top-[4px] left-0 mt-2 ml-3 inline-block">
+                          <svg viewBox="0 0 24 24" className="w-4 h-4">
+                            <path
+                              fill="#bbb"
+                              d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"
+                            />
+                          </svg>
+                        </span>
+                      </label>
+                    </div>
+                  </form>
+                </div>
+                <div className="flex flex-row justify-around w-full">
+                  <p className="ml-8">
+                    Join a{" "}
+                    <span
+                      onClick={() => setModlar(true)}
+                      className="text-sky-500 cursor-pointer"
+                    >
+                      Public
+                    </span>{" "}
+                    Group Chat
+                  </p>
+                  <div>
+                    <span className="" onClick={() => setCreateModlar(true)}>
+                      <Image
+                        width={24}
+                        height={24}
+                        src="/Chat/vector.svg"
+                        alt="create svg"
+                      />
+                    </span>
+                  </div>
+                </div>
+                <div
+                  className="contacts p-2 flex-1 overflow-y-scroll"
+                  onClick={(e) => {
+                    e.preventDefault();
                   }}
                 >
-                  <div className=" p-4 flex-none mt-4">
-                    <p
-                      className={`text-2xl font-bold md:block group-hover:block mb-4`}
-                    >
-                      Messages
-                    </p>
-                    <form onSubmit={(e) => e.preventDefault()}>
-                      <div className="relative sm:block hidden">
-                        <label>
-                          <input
-                            className="rounded-lg py-2 pr-6 pl-10 w-full bg-white focus:outline-none text-black focus:shadow-md transition duration-300 ease-in"
-                            type="text"
-                            placeholder="Search Messenger"
-                          />
-                          <span className="absolute top-[4px] left-0 mt-2 ml-3 inline-block">
-                            <svg viewBox="0 0 24 24" className="w-4 h-4">
-                              <path
-                                fill="#bbb"
-                                d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"
-                              />
-                            </svg>
-                          </span>
-                        </label>
-                      </div>
-                    </form>
-                  </div>
-                  <div className="flex flex-row justify-around w-full">
-                    <p className="ml-8">
-                      Join a{" "}
-                      <span
-                        onClick={() => setModlar(true)}
-                        className="text-sky-500 cursor-pointer"
-                      >
-                        Public
-                      </span>{" "}
-                      Group Chat
-                    </p>
-                    <div>
-                      <span className="" onClick={() => setCreateModlar(true)}>
-                        <Image
-                          width={24}
-                          height={24}
-                          src="/Chat/vector.svg"
-                          alt="create svg"
-                        />
-                      </span>
-                    </div>
-                  </div>
-                  <div
-                    className="contacts p-2 flex-1 overflow-y-scroll"
-                    onClick={(e) => {
-                      e.preventDefault();
-                    }}
-                  >
-                    {Array.isArray(chatList) &&
-                      chatList.map((value: any, key: any) => {
-                        return (
-                          <ChatCard
-                            key={key}
-                            swipe={setShowMessage}
-                            index={key}
-                            value={value}
-                            self={state.user}
-                            handleClick={handleSwitching}
-                            msg={value.msg}
-                          ></ChatCard>
-                        );
-                      })}
-                  </div>
-                </motion.section>
-                {children}
-              </main>
-            </div>
+                  {Array.isArray(chatList) &&
+                    chatList.map((value: any, key: any) => {
+                      return (
+                        <ChatCard
+                          key={key}
+                          swipe={setShowMessage}
+                          index={key}
+                          value={value}
+                          self={state.user}
+                          handleClick={handleSwitching}
+                          msg={value.msg}
+                        ></ChatCard>
+                      );
+                    })}
+                </div>
+              </motion.section>
+              {children}
+            </main>
           </div>
         </div>
       </div>
+    </div>
     // </QueryClientProvider>
   );
 };
