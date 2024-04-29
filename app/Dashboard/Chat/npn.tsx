880bf025ae725f436a0e3f7f487e0c6d2f56f126
amoq <abdelmottaliblol@gmail.com>
2024-04-29 08:37:05 +0100

    no scrollbar in chat

--- app/Dashboard/Chat/npn.tsx
+++ app/Dashboard/Chat/npn.tsx
@@ -141,7 +141,7 @@ const Page = ({ children }: { children: React.ReactNode }) => {
           <CreateChannel handleClick={() => setCreateModlar(false)} />
         ) : null}
       </AnimatePresence>
-      <div className="lg:max-h-[95%] lg:w-[91%] w-full h-full ">
+      <div className="lg:max-h-[95%]  lg:w-[91%] w-full h-full ">
         <div
           className={`h-full w-full flex antialiased text-gray-200 bg-primaryColor rounded-xl ${inter.className}`}
           style={{
@@ -152,7 +152,7 @@ const Page = ({ children }: { children: React.ReactNode }) => {
           <div className="flex-1 flex flex-col ">
             <main className="flex-grow flex flex-row min-h-0">
               <motion.section
-                className={` b flex flex-col flex-none overflow-auto ${
+                className={` b flex flex-col flex-none overflow-auto  ${
                   showMessage && tablet ? "invisible" : "visible"
                 } group lg:max-w-[300px] md:w-2/5 no-scrollbar`}
                 initial={{ display: "flex", width: "100%", opacity: 1 }}
@@ -212,7 +212,7 @@ const Page = ({ children }: { children: React.ReactNode }) => {
                   </div>
                 </div>
                 <div
-                  className="contacts p-2 flex-1 overflow-y-scroll"
+                  className="contacts p-2 flex-1 overflow-y-scroll no-scrollbar"
                   onClick={(e) => {
                     e.preventDefault();
                   }}
