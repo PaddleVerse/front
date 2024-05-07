a58529452bf1085925767b7c8b4b754c2ddef975
ybenlafk <yben0600@gmail.com>
2024-05-07 15:53:27 +0100

    :))

--- app/components/Dashboard/Overview/UserProfileSecond/UserProfileSecond.tsx
+++ app/components/Dashboard/Overview/UserProfileSecond/UserProfileSecond.tsx
@@ -12,6 +12,8 @@ import { useGlobalState } from "@/app/components/Sign/GlobalState";
 import axios from "axios";
 import { ipAdress } from "@/app/utils";
 const UserProfileSecond = ({ user }: any) => {
+  const [LoadingImg, setLoadingImg] = useState(false);
+  const [LoadingBi, setLoadingBi] = useState(false);
   const [topTopFriends, setTopTopFriends] = useState<any[]>([]);
   const [level, setLevel] = useState<number>(1);
   const [perc, setPerc] = useState<number>(0);
@@ -95,17 +97,31 @@ const UserProfileSecond = ({ user }: any) => {
           <PinContainer title="Overview" href="https://twitter.com/mannupaaji">
             <div className="overflow-hidden h-[290px] w-full relative">
               {user ? (
-                <Image
-                  src={
-                    user?.banner_picture ? user?.banner_picture : "/car1.jpg"
-                  }
-                  fill
-                  priority
-                  style={{ objectFit: "cover" }}
-                  alt="bg"
-                  sizes="auto"
-                  className="z-[-1] rounded-2xl"
-                />
+                <div className=" relative h-full w-full">
+                  <Image
+                    src={
+                      user?.banner_picture ? user?.banner_picture : "/car1.jpg"
+                    }
+                    fill
+                    priority
+                    onLoad={() => setLoadingBi(true)}
+                    style={{ objectFit: "cover" }}
+                    alt="bg"
+                    sizes="auto"
+                    className={`z-[-1] rounded-2xl ${LoadingBi ? 'block' : 'invisible'}`}
+                  />
+                  <div className={`absolute top-0 flex items-center justify-center w-full h-full  bg-gray-400 rounded  animate-pulse ${LoadingBi ? 'hidden' : 'block'}`}>
+                      <svg
+                          className="w-10 h-10 text-gray-300"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          viewBox="0 0 20 18"
+                      >
+                          <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+                      </svg>
+                  </div>
+                </div>
               ) : (
                 <div className="flex items-center justify-center w-full h-full bg-gray-400 rounded  animate-pulse">
                   <svg
@@ -125,14 +141,28 @@ const UserProfileSecond = ({ user }: any) => {
             <div className="w-full h-full flex flex-col items-center bg-primaryColor rounded-md gap-4">
               <div className="w-full h-[60%]   relative">
                 {user ? (
-                  <Image
-                    src={user?.picture ? user?.picture : "/b.png"}
-                    alt="profile"
-                    fill
-                    style={{ objectFit: "cover" }}
-                    sizes="auto"
-                    className="rounded-md"
-                  />
+                  <div className=" relative h-full w-full">
+                    <Image
+                      src={user?.picture ? user?.picture : "/b.png"}
+                      alt="profile"
+                      fill
+                      style={{ objectFit: "cover" }}
+                      sizes="auto"
+                      onLoad={() => setLoadingImg(true)}
+                      className={`rounded-md ${LoadingImg ? 'block' : 'invisible'}`}
+                    />
+                    <div className={`absolute top-0 flex items-center justify-center w-full h-full  bg-gray-400 rounded  animate-pulse ${LoadingImg ? 'hidden' : 'block'}`}>
+                        <svg
+                            className="w-10 h-10 text-gray-300"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 20 18"
+                        >
+                            <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+                        </svg>
+                    </div>
+                  </div>
                 ) : (
                   <div className="flex items-center justify-center w-full h-full bg-gray-400 rounded  animate-pulse">
                     <svg
--- app/components/Dashboard/Settings/EditProfile.tsx
+++ app/components/Dashboard/Settings/EditProfile.tsx
@@ -20,6 +20,7 @@ const EditProfile = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [preview, setPreview] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
+  const [LoadingImg, setLoadingImg] = useState(false);
   
   const refreshUser = async () => {
     try {
@@ -98,7 +99,38 @@ const EditProfile = () => {
 
       <div className='mt-20 flex items-center justify-between w-full border-b-[0.5px] border-white pb-10'>
         <div className='flex items-center'>
-          {user && <Image priority src={preview || user?.picture} alt='profile' width={200} height={200} className="object-cover h-[120px] w-[120px] rounded-full" />}
+          {user ?
+            <div className=' relative'>
+              <Image priority src={preview || user?.picture} alt='profile' 
+                width={200} height={200}
+                onLoad={() => setLoadingImg(true)}
+                className={`object-cover h-[120px] w-[120px] rounded-full ${LoadingImg ? 'block' : 'invisible'}`}
+              />
+              <div className={` absolute top-0 flex items-center justify-center bg-gray-400 h-[120px] w-[120px] rounded-full  animate-pulse ${LoadingImg ? 'hidden' : 'block'}`}>
+                <svg
+                  className="w-10 h-10 text-gray-300"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 18"
+                  >
+                  <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+                </svg>
+              </div>
+            </div>
+            :
+            <div className={`flex items-center justify-center bg-gray-400 h-[120px] w-[120px] rounded-full  animate-pulse`}>
+              <svg
+                className="w-10 h-10 text-gray-300"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 18"
+              >
+                <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+              </svg>
+            </div>
+          }
           <div className='flex flex-col gap-1 ml-10'>
             <h1 className='sm:text-md text-sm text-white font-light sm:flex hidden'>Profile Picture</h1>
             <p className='text-[#c2c2c2] text-sm font-light sm:flex hidden'>PNG, JPEG under 15MB</p>
