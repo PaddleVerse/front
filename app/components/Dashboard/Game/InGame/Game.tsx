e10bd9ddaa4ce0b89ea60dcef4757774f80a7f34
Anas Arbaoui <anas@arbaoui.me>
2024-05-20 18:51:12 +0100

    added oppponent id to frontend

--- app/components/Dashboard/Game/InGame/Game.tsx
+++ app/components/Dashboard/Game/InGame/Game.tsx
@@ -34,6 +34,7 @@ const GameCanvas: React.FC<GameCanvasProps> = ({ roomId }) => {
   const { user, socket } = state;
   const [score, setScore] = useState({ player1: 0, player2: 0 });
   const [winnerText, setWinnerText] = useState("");
+  const [opponent, setOpponent] = useState(0);
   const router = useRouter();
   const [end, setEnd] = useState(false);
   useEffect(() => {
@@ -63,13 +64,17 @@ const GameCanvas: React.FC<GameCanvasProps> = ({ roomId }) => {
           player2: newScore.score[1].score,
         });
       });
+      socket?.on("gameOpponent", (opponent: number) => {
+        setOpponent(opponent);
+      });
       socket?.on("endGame", (winner: any) => {
         setEnd(true);
         setWinnerText(winner.winner === userID ? "win" : "lost");
-        dispatch && dispatch({
-          type: "UPDATE_GAMESTATUS",
-          payload: winner.winner === userID ? "win" : "lose",
-        });
+        dispatch &&
+          dispatch({
+            type: "UPDATE_GAMESTATUS",
+            payload: winner.winner === userID ? "win" : "lose",
+          });
         dispatch && dispatch({ type: "UPDATE_GAMEINVITEID", payload: null });
         router.push("/Dashboard");
       });
@@ -134,7 +139,7 @@ const GameCanvas: React.FC<GameCanvasProps> = ({ roomId }) => {
     scene.add(new AmbientLighting(0xffffff, 0.1));
     let ball: Ball | null = null;
     fetchData(`/game/getUserBallSkin/${user?.id}`, "GET", null)
-      .then((res:any) => {
+      .then((res: any) => {
         if (!res) return;
         if (!res?.data) {
           ball = new Ball(
@@ -167,23 +172,41 @@ const GameCanvas: React.FC<GameCanvasProps> = ({ roomId }) => {
     let paddle: Paddle;
     let paddle2: Paddle;
     const res1 = fetchData(`/game/getUserPaddleSkin/${user?.id}`, "GET", null)
-    .then((res) => {
-      if (!res?.data) {
-        paddle = new Paddle(scene, { x: 16.0, y: 10.0, z: 0.0 }, (3 * Math.PI) / 2);
-        paddle2 = new Paddle(scene, { x: -16.0, y: 10.0, z: 0.0 }, Math.PI / 2);
-      } else {
-        let color : String = res.data.color.replace(user?.id, "");
-        // remove the user id from the color using something like remove
-        console.log("color: " + color);
-        paddle = new Paddle(scene, { x: 16.0, y: 10.0, z: 0.0 }, (3 * Math.PI) / 2, color);
-        paddle2 = new Paddle(scene, { x: -16.0, y: 10.0, z: 0.0 }, Math.PI / 2, color);
-      }
-      paddleRef.current = paddle;
-      paddle2Ref.current = paddle2;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+      .then((res) => {
+        if (!res?.data) {
+          paddle = new Paddle(
+            scene,
+            { x: 16.0, y: 10.0, z: 0.0 },
+            (3 * Math.PI) / 2
+          );
+          paddle2 = new Paddle(
+            scene,
+            { x: -16.0, y: 10.0, z: 0.0 },
+            Math.PI / 2
+          );
+        } else {
+          let color: String = res.data.color.replace(user?.id, "");
+          // remove the user id from the color using something like remove
+          console.log("color: " + color);
+          paddle = new Paddle(
+            scene,
+            { x: 16.0, y: 10.0, z: 0.0 },
+            (3 * Math.PI) / 2,
+            color
+          );
+          paddle2 = new Paddle(
+            scene,
+            { x: -16.0, y: 10.0, z: 0.0 },
+            Math.PI / 2,
+            color
+          );
+        }
+        paddleRef.current = paddle;
+        paddle2Ref.current = paddle2;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
     // Update the interval function
     const intervalId = setInterval(() => {
@@ -246,7 +269,7 @@ const GameCanvas: React.FC<GameCanvasProps> = ({ roomId }) => {
         camera.position.z = -(mouseX * 2);
       }
     };
-  
+
     mountRef.current?.addEventListener("mousemove", handleMouseMove);
 
     function animate() {
@@ -313,7 +336,9 @@ const GameCanvas: React.FC<GameCanvasProps> = ({ roomId }) => {
               rajdhani.className
             )}
           >
-            {score.player1} - {score.player2}
+            {/* give each id their score */}
+            
+            {user.id}: {score.player1} - {opponent}: {score.player2}
           </div>
         </div>
       )}
